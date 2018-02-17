a359dcde307b7e5b6690edf7af123d0c1a04cf0d
louisbichard <contact@louisjohnbichard.co.uk>
2018-02-17 19:33:14 +0000

    udpated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-git push -d origin gh-pages
-git branch -d gh-pages
-git subtree push --prefix _book origin gh-pages
+git push origin --delete gh-pages;
+git branch -d gh-pages;
+git subtree push --prefix _book origin gh-pages;
